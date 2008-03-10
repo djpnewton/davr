7dab172230ed9d294a5d61c790ec4775059b9f5e
djpnewton <djpnewton@4498daa2-c947-0410-8145-e1d1289b562a>
2008-03-10 03:31:58 +0000

    -added button to switch mode -still trying to add a beeper

--- trunk/motor_pmw/motor_pmw.c
+++ trunk/motor_pmw/motor_pmw.c
@@ -11,6 +11,11 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
+/* Mode */
+enum { MSTOP, MTRIMPOT, MDANCE };
+volatile int g_mode = MSTOP;
+
+void beep(int freq, int len);
 
 /* Timer defines */
 
@@ -83,21 +88,52 @@ void init_status_led()
     TCCR3B |= /*_BV(CS30) | _BV(CS31) |*/ _BV(CS32); // prescaler Fcpu / 64
 }
 
+void init_button_timer()
+{
+	// output PORTA.1
+	DDRA = 255;
+	//DDRA |= _BV(1);
+	DDRA &= ~_BV(3);
+	PORTA |= _BV(1);
+	//PINA = 0;
+
+	TCCR0 |= _BV(WGM01); // configure timer 0 for CTC mode
+	TIMSK |= _BV(OCIE0);
+	TIFR |= _BV(OCF0);
+	OCR0 = 255; 
+	TCCR0 |= _BV(CS02) | _BV(CS01) | _BV(CS00); // start timer at Fcpu / 1024
+}
+
+void init_piezo_timer()
+{
+	// output PORTF.5 and PORTF.3
+	DDRF |= _BV(5) | _BV(3);
+	PORTF |= _BV(5) | _BV(3);
+
+	TCCR2 |= _BV(WGM21); // configure timer 2 for CTC mode
+	TIMSK |= _BV(OCIE2);
+	TIFR |= _BV(OCF2);
+	OCR2 = 255; 
+	TCCR2 |= _BV(CS22) | _BV(CS20); // start timer at Fcpu / 1024
+}
+
 int main(void)
  {
     init_pwm();
     init_adc();
     init_status_led();
+	init_button_timer();
+	//init_piezo_timer();
 
     // turn off motors
     DDRG = 0xFF;
     PORTG = 0; 
 
      // turn on trimpot
-    DDRF = 2;
-    PORTF = 2;
-    DDRA = 255;
-    PORTA = 0;
+    DDRF |= 2;
+    PORTF |= 2;
+    DDRA |= _BV(7);
+    PORTA &= ~_BV(7);
 
     // Enable global interupts
     sei(); 
@@ -110,27 +146,138 @@ int main(void)
 
 ISR(ADC_vect)
 {
-    int adc = ADCH;
-    const float adc_max = 255;
-    const float adc_half = adc_max / 2.0;
 
-    if (adc <= adc_half)
-    {
-        PORTG = 5; // motors forward
+	if (g_mode == MSTOP)
+	{
+		OCRA = 0;
+		OCRC = 0;
+	}
+	else if (g_mode == MTRIMPOT)
+	{
+	    int adc = ADCH;
+	    const float adc_max = 255;
+	    const float adc_half = adc_max / 2.0;
 
-        OCRA = ((adc_half - adc) / adc_half) * (TIMER1_TOP);
-        OCRC = ((adc_half - adc) / adc_half) * (TIMER1_TOP);
-    }
-    else
-    {
-        PORTG = 10; // motors backward
+	    if (adc <= adc_half)
+	    {
+	        PORTG = 5; // motors forward
 
-        OCRA = ((adc - adc_half) / adc_half) * TIMER1_TOP;
-        OCRC = ((adc - adc_half) / adc_half) * TIMER1_TOP;
-    }
+	        OCRA = ((adc_half - adc) / adc_half) * (TIMER1_TOP);
+	        OCRC = ((adc_half - adc) / adc_half) * (TIMER1_TOP);
+	    }
+	    else
+	    {
+	        PORTG = 10; // motors backward
+
+	        OCRA = ((adc - adc_half) / adc_half) * TIMER1_TOP;
+	        OCRC = ((adc - adc_half) / adc_half) * TIMER1_TOP;
+	    }
+	}
 }
 
-ISR (TIMER1_OVF_vect)
+ISR(TIMER1_OVF_vect)
 {
 
+}
+
+enum { LEFT, RIGHT };
+enum { STOP = 0, SLOW = 700, MED = 800, FAST = 1000 };
+volatile uint16_t g_dance_cnt = 0;
+enum { D1 = 0, D2 = 100, D3 = 200, D4 = 250, D5 = 450, DSTOP = 600 };
+volatile int g_mode_switch = 0;
+
+void motor_speed(int motor, int speed)
+{
+	switch (motor)
+	{
+		case LEFT:
+			OCRA = speed;
+			return;
+		case RIGHT:
+			OCRC = speed;
+			return;
+	}
+}
+
+
+ISR(TIMER0_COMP_vect)
+{
+	// if button is pressed change gMode
+	if (PINA & _BV(3))
+		g_mode_switch = 1;
+	else if (g_mode_switch)
+	{
+		g_mode++;
+		if (g_mode > MDANCE)
+			g_mode = MSTOP;
+		else if (g_mode == MDANCE)
+		{
+			PORTG = 5; // motors forward
+			g_dance_cnt = 0; // reset dance counter
+		}
+		g_mode_switch = 0;
+
+		beep(100 + g_mode * 10, 250);
+	}
+
+	// reset flag so interupt gets called again
+	TIFR |= _BV(OCF0);
+
+	// dance routine
+	if (g_mode == MDANCE)
+	{
+		switch (g_dance_cnt)
+		{
+			case D1:
+				motor_speed(LEFT, MED);
+				motor_speed(RIGHT, MED);
+				break;
+			case D2:
+				motor_speed(LEFT, STOP);
+				motor_speed(RIGHT, MED);
+				break;
+			case D3:
+				motor_speed(LEFT, STOP);
+				motor_speed(RIGHT, STOP);
+				break;
+			case D4:
+				motor_speed(LEFT, FAST);
+				motor_speed(RIGHT, MED);
+				break;
+			case D5:
+				motor_speed(LEFT, FAST);
+				motor_speed(RIGHT, STOP);
+				break;
+		}
+		if (g_dance_cnt >= DSTOP)
+		{
+			motor_speed(LEFT, STOP);
+			motor_speed(RIGHT, STOP);
+		}
+		g_dance_cnt++;
+	}
+}
+
+volatile int g_beep_count;
+
+void beep(int freq, int len)
+{
+	//OCR2 = freq;
+	//g_beep_count = len;
+}
+
+ISR(TIMER2_COMP_vect)
+{
+	// reset flag so interupt gets called again
+	TIFR |= _BV(OCF2);
+
+	if (g_beep_count > 0)
+	{
+		// toggle beeper pin
+		PORTF &= ~_BV(5);
+
+		g_beep_count--;
+	}
+	else if (g_beep_count == 0)
+		PORTF |= _BV(5);
 }
